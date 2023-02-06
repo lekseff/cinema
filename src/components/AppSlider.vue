b058815e6369733731c64bf8206e0b152a39ea05
Aleksey Zayachnikov <lekseff@gmail.com>
2023-02-06 14:55:55 +0300

    slider ok

--- src/components/AppSlider.vue
+++ src/components/AppSlider.vue
@@ -1,17 +1,24 @@
 <template>
   <v-carousel
       cycle
-      height="530"
+      height="auto"
       hide-delimiter-background
-      show-arrows="hover"
+      :show-arrows="isShowArrows"
       class="rounded-lg"
   >
     <v-carousel-item
         v-for="(slide, i) in slides"
         :key="i"
-        :src="slide.src"
-        cover
     >
+      <router-link :to="{name: 'ShowMovie', params: {id: slide.movieId}}">
+        <v-img
+            :src="slide.src"
+            cover
+            aspect-ratio="2.17"
+        >
+
+        </v-img>
+      </router-link>
     </v-carousel-item>
   </v-carousel>
 </template>
@@ -22,22 +29,37 @@ export default {
   data: () => ({
     slides: [
       {
+        id: 1011,
+        movieId: 1011,
         src: 'img/slider/divergent.jpg'
       },
       {
+        id: 1012,
+        movieId: 1012,
         src: 'img/slider/inception.jpg'
       },
       {
+        id: 1013,
+        movieId: 1013,
         src: 'img/slider/tour.jpg'
       },
       {
+        id: 1014,
+        movieId: 1014,
         src: 'img/slider/joker.jpg'
       },
       {
+        id: 1015,
+        movieId: 1015,
         src: 'img/slider/ring.jpg'
       }
     ]
-  })
+  }),
+  computed: {
+    isShowArrows() {
+      return this.$vuetify.display.smAndDown ? false : 'hover'
+    }
+  }
 }
 </script>
 
