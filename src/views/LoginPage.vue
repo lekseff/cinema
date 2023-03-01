36c707aaced12fb9517b6a59e6702f1f6be00851
Aleksey Zayachnikov <lekseff@gmail.com>
2023-03-02 00:48:31 +0300

    Сделал получение cookie

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -14,7 +14,7 @@
              class="d-flex flex-column px-5 py-4"
          >
             <v-text-field
-                v-model="email"
+                v-model="formData.email"
                 clearable
                 type="email"
                 label="Email"
@@ -27,7 +27,7 @@
             >
             </v-text-field>
             <v-text-field
-                v-model="password"
+                v-model="formData.password"
                 clearable
                 class="mt-3"
                 maxlength="50"
@@ -42,26 +42,92 @@
             <v-btn class="align-self-center" color="deep-orange-lighten-1" @click="validate">ВОЙТИ</v-btn>
          </v-form>
       </v-card>
+
+<!--      <v-btn-->
+<!--         @click="onTest"-->
+<!--      >-->
+<!--         Test-->
+<!--      </v-btn>-->
    </div>
 </template>
 
 <script>
 import {rules} from '@/../constants'
+import axios from 'axios'
 
 export default {
    name: "AppLogin",
    data: () => ({
-
-      email: '',
-      password: '',
-      rules: rules
+      rules: rules,
+      formData: {
+         email: '',
+         password: ''
+      }
    }),
    methods: {
+      // async onTest() {
+      //    const url = process.env.VUE_APP_API_URL
+      //    try {
+      //       axios.defaults.withCredentials = true
+      //       const response = await axios.get(`${url}/api/test`, {
+      //          headers: {
+      //             'Accept': 'application/json'
+      //          }
+      //       })
+      //       console.log('get', response)
+      //    } catch (e) {
+      //       console.log('get', e)
+      //    }
+      // },
+
       async validate() {
          const {valid} = await this.$refs.loginForm.validate()
          if (!valid) return
-         console.log('Форма валидна, отправляем')
-         console.log()
+         const url = process.env.VUE_APP_API_URL
+         axios.defaults.withCredentials = true
+         try {
+            //   Запрос на получение cookie и токена
+            await axios(`${url}/sanctum/csrf-cookie`, {
+               headers: {
+                  'Accept': 'application/json',
+               },
+            })
+            await axios.post(`${url}/login`, this.formData, {
+               headers: {
+                  'Accept': 'application/json',
+                  'X-Requested-With': 'XMLHttpRequest',
+                  'Access-Control-Allow-Origin': '*',
+               },
+            })
+                .then(res => {
+                   console.log(res.config.headers['X-XSRF-TOKEN'])
+                   this.$router.push({name: 'dashboard'})
+                })
+         } catch (e) {
+            console.log(e)
+         }
+         // axios.get(`${url}/sanctum/csrf-cookie`,
+         //     // {
+         //     //    withCredentials: true,
+         //     //    headers: {
+         //     //       'Accept': 'Application/json',
+         //     //       'X-Requested-With': 'XMLHttpRequest',
+         //     //       'Access-Control-Allow-Origin': '*',
+         //     //    },
+         //     // }
+         // )
+         //     .then((res) => {
+         //        console.log('res csrf', res)
+         //
+         //        // axios.post(`${url}/login`, this.formData, {
+         //        //    withCredentials: true,
+         //        //    headers: {
+         //        //       'Accept': 'Application/json'
+         //        //    },
+         //        // }).then(response => {
+         //        //    console.log(response)
+         //        // })
+         //     })
          // this.$router.push({name: 'dashboard'})
       },
    }
